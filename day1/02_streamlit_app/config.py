77b46c5c2982524072ccd70ebce7d92cddac8c1a
oooNEHANooo <ooonahenooo@gmail.com>
2025-04-23 08:18:51 +0000

    feat: Add SakanaAI/TinySwallow-1.5B model

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,4 +1,5 @@
 # config.py
 DB_FILE = "chat_feedback.db"
 # MODEL_NAME = "google/gemma-2-2b-jpn-it"
-MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+# MODEL_NAME = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+MODEL_NAME = "SakanaAI/TinySwallow-1.5B"
